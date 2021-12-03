a60a06d7304c768cb9218fdc347b7fca27d33c4b
allisonvinicius <allisonvinicius63@gmail.com>
2021-12-03 15:45:07 -0400

    -view

---
+++ crudnative/src/App.js
@@ -0,0 +1,20 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import React from 'react';
+import UserForm from './views/UserForm';
+import UserList from './views/UserList';
+
+const Stack = createStackNavigator();
+
+export default props => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="UserList"
+        screenOptions={screenOptions}>
+        <Stack.Screen name="UserList" component={UserList} />
+        <Stack.Screen name="UserForm" component={UserForm} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
