f18915ffbc3216a1b6ccaee6df3ed844596a7258
allisonvinicius <allisonvinicius63@gmail.com>
2021-12-03 21:16:33 -0400

    -config

--- crudnative/src/App.js
+++ crudnative/src/App.js
@@ -1,6 +1,7 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
+import {Button, Icon} from 'react-native-elements';
 import UserForm from './views/UserForm';
 import UserList from './views/UserList';
 
@@ -12,11 +13,37 @@ export default props => {
       <Stack.Navigator
         initialRouteName="UserList"
         screenOptions={screenOptions}>
-        <Stack.Screen name="UserList" component={UserList} />
-        <Stack.Screen name="UserForm" component={UserForm} />
+        <Stack.Screen
+          name="UserList"
+          component={UserList}
+          options={() => {
+            return {
+              title: 'Lista de Usuarios',
+              headerRight: () => (
+                <Button
+                  type="clear"
+                  icon={<Icon name="add" size={25} color="white" />}
+                />
+              ),
+            };
+          }}
+        />
+        <Stack.Screen
+          name="UserForm"
+          component={UserForm}
+          options={{
+            title: 'Formulário de Usuários',
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
-const screenOptions = {};
+const screenOptions = {
+  headerStyle: {
+    backgroundColor: '#f4511e',
+  },
+  headerTintColor: '#fff',
+  headerTitleStyle: {},
+};
