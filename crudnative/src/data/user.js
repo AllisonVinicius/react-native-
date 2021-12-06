e40bbd532e313bcff691aae97174c10aa64fb76d
allisonvinicius <allisonvinicius63@gmail.com>
2021-12-05 20:02:17 -0400

    -mock

---
+++ crudnative/src/data/user.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 1,
+    name: 'Teste',
+    email: 'teste@gmail.com',
+    avatarUrl:
+      'https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_960_720.png',
+  },
+  {
+    id: 2,
+    name: 'Agendor',
+    email: 'agenor@gmail.com',
+    avatarUrl:
+      'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png',
+  },
+  {
+    id: 3,
+    name: 'duque',
+    email: 'duque@gmail.com',
+    avatarUrl:
+      'https://cdn.pixabay.com/photo/2018/08/28/12/41/avatar-3637425_960_720.png',
+  },
+];
