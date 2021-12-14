fe948082345a6e86c6b9f2bd20b520503496d2d5
allisonvinicius <allisonvinicius63@gmail.com>
2021-12-14 10:47:42 -0400

    -delet-user-function

--- crudnative/src/views/UserList.js
+++ crudnative/src/views/UserList.js
@@ -1,9 +1,40 @@
 import React from 'react';
-import {FlatList, View} from 'react-native';
-import {ListItem} from 'react-native-elements';
+import {Alert, FlatList, View} from 'react-native';
+import {Button, Icon, ListItem} from 'react-native-elements';
 import users from '../data/users';
 
 export default props => {
+  function confirmUserDeletion(user) {
+    Alert.alert('Excluir Usuário', 'Deseja Excluir o usuario', [
+      {
+        text: 'Sim',
+        onPress() {
+          console.warn('delete');
+        },
+      },
+      {
+        text: 'Nao',
+      },
+    ]);
+  }
+
+  function getActions(user) {
+    return (
+      <>
+        <Button
+          onPress={() => props.navigation.navigate('UserForm', user)}
+          type="clear"
+          icon={<Icon name="edit" size={25} color="orange" />}
+        />
+        <Button
+          onPress={() => confirmUserDeletion(user)}
+          type="clear"
+          icon={<Icon name="delete" size={25} color="red" />}
+        />
+      </>
+    );
+  }
+
   function getUserItem({item: user}) {
     return (
       <ListItem
@@ -12,6 +43,7 @@ export default props => {
         title={user.name}
         subtitle={user.email}
         bottomDivider
+        rightElement={getActions(user)}
         onPress={() => props.navigation.navigate('UserForm')}
       />
     );
